e3a3a7e1f0bfac373bc570818dd9687f250127e9
snotbug <you@example.com>
2023-07-25 11:17:15 -0700

    no

--- search.c
+++ search.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    FILE *dictionary = fopen("cedict_ts.u8", "r");
+    FILE *dictionary = fopen("cedict_ts.u8.bak", "r");
     if(dictionary == NULL)
     {
         printf("error opening dictionary\n");
@@ -30,16 +30,14 @@ int main(int argc, char *argv[])
     {
         entries[i].numDefinitions = 0;
     }
+    
+    char traditional[100];
+    char simplified[100];
+    char pinyin[500];
+    char definitions[1000];
 
-    char buffer[1000];
-    while(fgets(buffer, sizeof(buffer), dictionary) != NULL)
+    while(fscanf(dictionary, "%s %s [%[^]]] %[^\n]", traditional, simplified, pinyin, definitions) == 4)
     {
-        char traditional[100];
-        char simplified[100];
-        char pinyin[500];
-
-        sscanf(buffer, "%99s %99s [%499[^]]] /", traditional, simplified, pinyin);
-
         for(int i = 0; i < argc - 1; i++)
         {
             if(strcmp(traditional, argv[i + 1]) == 0 || strcmp(simplified, argv[i + 1]) == 0)
@@ -48,8 +46,7 @@ int main(int argc, char *argv[])
                 strcpy(entries[i].simplified, simplified);
                 strcpy(entries[i].pinyin, pinyin);
 
-                char *token = strtok(buffer, "/");
-                token = strtok(NULL, "/");
+                char *token = strtok(definitions, "/");
                 while(token != NULL)
                 {
                     strcpy(entries[i].definitions[entries[i].numDefinitions], token);
